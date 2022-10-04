f18584b1ac0357eef94dc58e6ff3492d9ec07012
0525aa36 <0525aa36@gmail.com>
2022-10-04 23:51:06 +0900

    Initial commit

---
+++ src/main/java/dew/be/profile/controller/DemoController.java
@@ -0,0 +1,17 @@
+package dew.be.profile.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @Value("${db_id}")
+    private String db_id;
+    @GetMapping("/demo")
+    public String demo() {
+        System.out.println(db_id);
+        return  "demo";
+    }
+}
